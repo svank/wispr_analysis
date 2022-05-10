2800a6e78fb0190b5deb77b1d4a375d024f6f97e
Sam Van Kooten <vankooten.sam@gmail.com>
2022-05-09 18:40:36 -0600

    Expand testing, docs, and options in composites.py

--- composites.py
+++ composites.py
@@ -1,6 +1,5 @@
 from collections.abc import Iterable
 import copy
-import warnings
 
 from astropy.io import fits
 import astropy.units as u
@@ -119,17 +118,46 @@ def find_collective_bounds(hdrs, wcs_target, trim=(0, 0, 0, 0), key=' '):
 
 
 def gen_header(hdr_i, hdr_o, proj='ARC', level=False, key=' '):
+    """
+    Generates a header suitable for a WISPR compositte field of view.
+    
+    This is mostly a port from pyssw.
+    
+    The output WCS will have a reference pixel near the center of the composite
+    field of view, with other values set accordingly.
+    
+    Arguments
+    ---------
+    hdr_i, hdr_o : astropy.io.fits.Header
+        The FITS headers for an inner-FOV and outer-FOV image
+    proj : str
+        The output projection to use
+    level : boolean
+        If `True`, the output coordinate system is shifted so that the lines of
+        zero degrees latitude and longitude appear straight. Intended for use
+        with helioprojective frames only.
+    key : str
+        The coordinate system associated with this key value will be loaded
+        from the headers.
+    
+    Returns
+    -------
+    wcsh : WCS
+        The output WCS object
+    naxis1, naxis2 : int
+        The size of the output image
+    """
     naxis1, naxis2 = int(hdr_i['NAXIS1'] * 3), int(hdr_i['NAXIS2'] * 2)
-    with warnings.catch_warnings():
-        warnings.filterwarnings(
-                action='ignore', message=".*'datfix' made the change.*")
+    with utils.ignore_fits_warnings():
         wcs_i = WCS(hdr_i, key=key)
         wcs_o = WCS(hdr_o, key=key)
 
     crval1 = (wcs_i.wcs.crval[0] + (wcs_o.wcs.crval[0] * 1.15)) / 2.
     crval2 = (wcs_i.wcs.crval[1] + wcs_o.wcs.crval[1]) / 2.
-    ctype1, ctype2 = (f"{wcs_i.wcs.ctype[0].split('-')[0]}-{proj}",
-            f"{wcs_i.wcs.ctype[1].split('-')[0]}-{proj}")
+    ctype1 = wcs_i.wcs.ctype[0].split('-')[0]
+    ctype2 = wcs_i.wcs.ctype[1].split('-')[0]
+    ctype1 += '-' * (5 - len(ctype1)) + proj
+    ctype2 += '-' * (5 - len(ctype2)) + proj
 
     crpix1 = naxis1 / 2
     crpix2 = naxis2 / 2
@@ -155,12 +183,66 @@ def gen_header(hdr_i, hdr_o, proj='ARC', level=False, key=' '):
     return wcsh, naxis1, naxis2
 
 
-def gen_composite(fname_i, fname_o, proj='ARC', level=False,
+def gen_composite(fname_i, fname_o, proj='ARC', level=False, key=' ',
         return_both=False, bounds=None, wcsh=None, naxis1=None, naxis2=None,
-        blank_i=False, blank_o=False, **kwargs):
-    with warnings.catch_warnings():
-        warnings.filterwarnings(
-                action='ignore', message=".*'BLANK' keyword.*")
+        blank_i=False, blank_o=False,
+        image_trim='auto', **kwargs):
+    """
+    Generates a composite image from two WISPR images (inner and outer)
+    
+    Arguments
+    ---------
+    fname_i, fname_o : str
+        The file names of the inner and outer images
+    proj : str
+        The projection to use for the composite field of view
+    level : boolean
+        If `True`, the output coordinate system is shifted so that the lines of
+        zero degrees latitude and longitude appear straight. Intended for use
+        with helioprojective frames only.
+    key : str
+        The coordinate system associated with this key value will be loaded
+        from the headers.
+    return_both : booean
+        If ``True``, the two individual images are reprojected into the common
+        field of view but are not combined. Instead, two separate, reprojected
+        images are returned.
+    bounds : tuple
+        The composite image is cropped at these pixel locations. The ordering
+        is (left, right, bottom, top). If None, the bounds are autodetected to
+        tightly bound the reprojected image data. If False, cropping is not
+        performed.
+    wcsh : WCS
+        The coordinate system for the composite field of view. If not provided,
+        the system is automatically generated.
+    naxis1, naxis2 : int
+        If ``wcsh`` is provided, the size of the output image must also be
+        provided with these two values.
+    blank_i, blank_o : boolean
+        Set either flag to ``True`` to not render the inner or outer image.
+        Files must still be provided for any images not drawn so that the
+        proper size of the output image can be determined. This flag is inteded
+        for rendering frames in an animation, when some frames should still
+        render a full field of view but one imager is temporarily not being
+        shown.
+    image_trim : Iterable
+        The edges of the input images are trimmed. ``image_trim`` is a list of
+        two lists, one for the inner and outer images, respectively. Each
+        sub-list is a number of pixels to trim from the left, right, bottom,
+        and top edges, respectively. Any value can be ``None`` to use the
+        default for that value. Set to ``False`` to disable trimming. The
+        default is sensible values for typical L3 images.
+    kwargs
+        Any remaining arguments are passed to `reproject.reproject_adaptive`.
+    
+    Returns
+    -------
+    composite : np.ndarray
+        The composite image array
+    wcsh : WCS
+        The coordinate system for the composite image
+    """
+    with utils.ignore_fits_warnings():
         if isinstance(fname_i, tuple):
             img_i, hdr_i = fname_i
             hdr_i = hdr_i.copy()
@@ -174,16 +256,23 @@ def gen_composite(fname_i, fname_o, proj='ARC', level=False,
     
     if wcsh is None:
         wcsh, naxis1, naxis2 = gen_header(
-                hdr_i, hdr_o, proj=proj, level=level)
+                hdr_i, hdr_o, proj=proj, level=level, key=key)
     else:
         wcsh = wcsh.deepcopy()
     
-    # Pixels to trim from the image edges.
-    # (outer, inner), then (left, right, bottom, top)
-    xy_bounds = [[20, 25, 0, 0], [33, 40, 42, 39]]
+    image_trim_default = [[20, 25, 0, 0], [33, 40, 42, 39]]
+    if not image_trim:
+        image_trim = [[0] * 4] * 2
+    if image_trim == 'auto':
+        image_trim = image_trim_default
+    
+    for i in range(len(image_trim)):
+        for j in range(len(image_trim[i])):
+            if image_trim[i][j] is None:
+                image_trim[i][j] = image_trim_default[i][j]
     
     imgs = []
-    for img, hdr, trim in zip((img_o, img_i), (hdr_o, hdr_i), xy_bounds):
+    for img, hdr, trim in zip((img_o, img_i), (hdr_o, hdr_i), image_trim[::-1]):
         imgs.append(img[trim[2]:img.shape[0]-trim[3],
                         trim[0]:img.shape[1]-trim[1]])
         hdr['NAXIS1'] -= trim[0] + trim[1]
@@ -193,8 +282,8 @@ def gen_composite(fname_i, fname_o, proj='ARC', level=False,
     img_o, img_i = imgs
     
     if bounds is None:
-        img_i_bounds = find_bounds(hdr_i, wcsh)
-        img_o_bounds = find_bounds(hdr_o, wcsh)
+        img_i_bounds = find_bounds(hdr_i, wcsh, key=key)
+        img_o_bounds = find_bounds(hdr_o, wcsh, key=key)
         bounds = (min(img_i_bounds[0], img_o_bounds[0]),
                   max(img_i_bounds[1], img_o_bounds[1]),
                   min(img_i_bounds[2], img_o_bounds[2]),
@@ -213,28 +302,27 @@ def gen_composite(fname_i, fname_o, proj='ARC', level=False,
         for k in 'date-obs', 'rsun_ref', 'dsun_obs', 'crln_obs', 'crlt_obs':
             del hdr[k]
     
-    with warnings.catch_warnings():
-        warnings.filterwarnings(
-                action='ignore', message=".*'datfix' made the change.*")
+    with utils.ignore_fits_warnings():
         if blank_i:
             o1 = np.full((naxis2, naxis1), np.nan)
         else:
             o1 = reproject.reproject_adaptive(
-                    (img_i, hdr_i), wcsh, (naxis2, naxis1),
+                    (img_i, WCS(hdr_i, key=key)), wcsh, (naxis2, naxis1),
                     roundtrip_coords=False, return_footprint=False,
                     boundary_mode='ignore_threshold', **kwargs)
         if blank_o:
             o2 = np.full((naxis2, naxis1), np.nan)
         else:
             o2 = reproject.reproject_adaptive(
-                    (img_o, hdr_o), wcsh, (naxis2, naxis1),
+                    (img_o, WCS(hdr_o, key=key)), wcsh, (naxis2, naxis1),
                     roundtrip_coords=False, return_footprint=False,
                     boundary_mode='ignore_threshold', **kwargs)
     if return_both:
         return o1, o2, wcsh
     replace = np.isnan(o1)
-    o1[replace] = o2[replace]
-    return o1, wcsh
+    composite = o1
+    composite[replace] = o2[replace]
+    return composite, wcsh
 
 
 def plot_map(map, wcsh, figsize=(15, 10)):
--- tests/test_composites.py
+++ tests/test_composites.py
@@ -1,9 +1,24 @@
-from .. import composites
+from .. import composites, plot_utils, utils
 
+from copy import deepcopy
+import os
+
+from astropy.io import fits
 from astropy.wcs import WCS
+import matplotlib.pyplot as plt
+import numpy as np
+import pytest
+import warnings
 
 
-def test_find_bounds():
+@pytest.mark.parametrize('wcs_key', [' ', 'A'])
+def test_find_bounds(wcs_key):
+    wcs_out = WCS(naxis=2)
+    wcs_out.wcs.crpix = 1, 1
+    wcs_out.wcs.crval = 0, 0
+    wcs_out.wcs.cdelt = 1, 1
+    wcs_out.wcs.ctype = "HPLN-CAR", "HPLT-CAR"
+    
     wcs_in = WCS(naxis=2)
     wcs_in.wcs.crpix = 1, 1
     # Add an offset to avoid anything landing right at pixel boundaries and so
@@ -12,20 +27,211 @@ def test_find_bounds():
     wcs_in.wcs.cdelt = 1, 1
     wcs_in.wcs.ctype = "HPLN-CAR", "HPLT-CAR"
     
+    header = wcs_in.to_header(key=wcs_key)
+    header['NAXIS1'] = 10
+    header['NAXIS2'] = 12
+    
+    bounds = composites.find_bounds(header, wcs_out, key=wcs_key)
+    
+    assert bounds == (0, 10, 0, 12)
+    
+    bounds = composites.find_bounds(header, wcs_out, trim=(1,2,4,5),
+            key=wcs_key)
+    
+    assert bounds == (1, 8, 4, 7)
+
+
+@pytest.mark.parametrize('wcs_key', [' ', 'A'])
+def test_find_collective_bounds(wcs_key):
     wcs_out = WCS(naxis=2)
     wcs_out.wcs.crpix = 1, 1
     wcs_out.wcs.crval = 0, 0
     wcs_out.wcs.cdelt = 1, 1
     wcs_out.wcs.ctype = "HPLN-CAR", "HPLT-CAR"
     
-    header = wcs_in.to_header()
-    header['NAXIS1'] = 10
-    header['NAXIS2'] = 12
+    wcs_in = WCS(naxis=2)
+    wcs_in.wcs.crpix = 1, 1
+    # Add an offset to avoid anything landing right at pixel boundaries and so
+    # having to care about floating-point error
+    wcs_in.wcs.crval = 0.1, 0.1
+    wcs_in.wcs.cdelt = 1, 1
+    wcs_in.wcs.ctype = "HPLN-CAR", "HPLT-CAR"
     
-    bounds = composites.find_bounds(header, wcs_out)
+    header1 = wcs_in.to_header(key=wcs_key)
+    header1['NAXIS1'] = 10
+    header1['NAXIS2'] = 12
     
-    assert bounds == (0, 10, 0, 12)
+    header2 = deepcopy(header1)
+    header2['NAXIS2'] = 24
     
-    bounds = composites.find_bounds(header, wcs_out, trim=(1,2,4,5))
+    bounds = composites.find_collective_bounds([header1, header2], wcs_out,
+            key=wcs_key)
+    # Bounds are (0, 10, 0, 12) and (0, 10, 0, 24)
+    assert bounds == (0, 10, 0, 24)
     
+    header2['CRPIX1' + wcs_key] = 2
+    header2['CRPIX2' + wcs_key] = 3
+    
+    bounds = composites.find_collective_bounds([header1, header2], wcs_out,
+            key=wcs_key)
+    # Bounds are (0, 10, 0, 12) and (-1, 9, -2, 22)
+    assert bounds == (-1, 10, -2, 22)
+    
+    # Test `trim` values
+    bounds = composites.find_collective_bounds([header1, header2], wcs_out,
+            trim=(1, 2, 4, 5), key=wcs_key)
+    # Bounds are (1, 8, 4, 7) and (0, 7, 2, 17)
+    assert bounds == (0, 8, 2, 17)
+    
+    # Test multiple sub-lists and only one trim value to apply to each
+    bounds = composites.find_collective_bounds([[header1], [header2]], wcs_out,
+            trim=(1, 2, 4, 5), key=wcs_key)
+    # Bounds are (1, 8, 4, 7) and (0, 7, 2, 17)
+    assert bounds == (0, 8, 2, 17)
+    
+    # Test multiple sub-lists and a separate trim value to apply to each
+    bounds = composites.find_collective_bounds([[header1], [header2]], wcs_out,
+            trim=[(0, 0, 0, 0), (1, 2, 4, 5)], key=wcs_key)
+    # Bounds are (0, 10, 0, 12) and (0, 7, 2, 17)
+    assert bounds == (0, 10, 0, 17)
+    
+    bounds = composites.find_collective_bounds([[header1], [header2]], wcs_out,
+            trim=[(1, 2, 4, 5), (0, 0, 0, 0)], key=wcs_key)
+    # Bounds are (1, 8, 4, 7) and (-1, 9, -2, 22)
+    assert bounds == (-1, 9, -2, 22)
+    
+    # Finally test just one header
+    bounds = composites.find_collective_bounds([header1], wcs_out,
+            trim=(1, 2, 4, 5), key=wcs_key)
     assert bounds == (1, 8, 4, 7)
+    bounds = composites.find_collective_bounds(header1, wcs_out,
+            trim=(1, 2, 4, 5), key=wcs_key)
+    assert bounds == (1, 8, 4, 7)
+
+
+def test_gen_header():
+    dir_path = (os.path.dirname(__file__)
+                + '/test_data/WISPR_files_with_data_half_size/')
+    ifiles, ofiles = utils.collect_files(dir_path)
+    with utils.ignore_fits_warnings():
+        hdr_i = fits.getheader(ifiles[0])
+        hdr_o = fits.getheader(ofiles[0])
+    
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore',
+                '.*has more axes \\(2\\) than the image.*')
+        wcs, naxis1, naxis2 = composites.gen_header(hdr_i, hdr_o)
+    assert list(wcs.wcs.ctype) == ['HPLN-ARC', 'HPLT-ARC']
+    assert wcs.wcs.crval[0] == pytest.approx(60.013157)
+    assert wcs.wcs.crval[1] == pytest.approx(-10.51129)
+    assert list(wcs.wcs.crpix) == [720, 512]
+    assert list(wcs.wcs.cdelt) == [0.08461, 0.08461]
+    assert (naxis1, naxis2) == (1440, 1024)
+    
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore',
+                '.*has more axes \\(2\\) than the image.*')
+        wcs, naxis1, naxis2 = composites.gen_header(hdr_i, hdr_o, key='A',
+                proj='CAR')
+    assert list(wcs.wcs.ctype) == ['RA---CAR', 'DEC--CAR']
+    assert wcs.wcs.crval[0] == pytest.approx(207.78723)
+    assert wcs.wcs.crval[1] == pytest.approx(-10.59083)
+    assert list(wcs.wcs.crpix) == [720, 512]
+    assert list(wcs.wcs.cdelt) == [-0.08461, 0.08461]
+    assert (naxis1, naxis2) == (1440, 1024)
+
+
+def setup_gen_composite_test():
+    dir_path = (os.path.dirname(__file__)
+                + '/test_data/WISPR_files_with_data_half_size/')
+    ifiles, ofiles = utils.collect_files(dir_path)
+    
+    with utils.ignore_fits_warnings():
+        hdr_i = fits.getheader(ifiles[0])
+        hdr_o = fits.getheader(ofiles[0])
+    
+    return ifiles[0], ofiles[0], hdr_i, hdr_o
+
+
+@pytest.mark.mpl_image_compare()
+def test_gen_composite():
+    ifile, ofile, hdr_i, hdr_o = setup_gen_composite_test()
+    
+    # Disable any trimming or cropping to make it easy to validate the WCS
+    composite, wcs = composites.gen_composite(ifile, ofile,
+            image_trim=False, bounds=False)
+    
+    assert (wcs.to_header_string()
+            == composites.gen_header(hdr_i, hdr_o)[0].to_header_string())
+    
+    fig = plt.figure()
+    plot_utils.plot_WISPR(composite, wcs=wcs, grid=1)
+    return fig
+
+
+@pytest.mark.mpl_image_compare()
+def test_gen_composite_with_auto_trim_crop():
+    ifile, ofile, hdr_i, hdr_o = setup_gen_composite_test()
+    
+    composite, wcs = composites.gen_composite(ifile, ofile)
+    
+    fig = plt.figure()
+    plot_utils.plot_WISPR(composite, wcs=wcs, grid=1)
+    return fig
+
+
+@pytest.mark.mpl_image_compare()
+def test_gen_composite_blank_i():
+    ifile, ofile, hdr_i, hdr_o = setup_gen_composite_test()
+    
+    composite, wcs = composites.gen_composite(ifile, ofile, blank_i=True)
+    
+    fig = plt.figure()
+    plot_utils.plot_WISPR(composite, wcs=wcs, grid=1)
+    return fig
+
+
+@pytest.mark.mpl_image_compare()
+def test_gen_composite_blank_o():
+    ifile, ofile, hdr_i, hdr_o = setup_gen_composite_test()
+    
+    composite, wcs = composites.gen_composite(ifile, ofile, blank_o=True)
+    
+    fig = plt.figure()
+    plot_utils.plot_WISPR(composite, wcs=wcs, grid=1)
+    return fig
+
+
+@pytest.mark.mpl_image_compare()
+def test_gen_composite_ra_dec():
+    ifile, ofile, hdr_i, hdr_o = setup_gen_composite_test()
+    
+    composite, wcs = composites.gen_composite(ifile, ofile, key='A')
+    
+    fig = plt.figure()
+    plot_utils.plot_WISPR(composite, wcs=wcs, grid=1)
+    return fig
+
+
+@pytest.mark.mpl_image_compare()
+def test_gen_composite_custom_trim():
+    ifile, ofile, hdr_i, hdr_o = setup_gen_composite_test()
+    
+    composite, wcs = composites.gen_composite(ifile, ofile,
+            image_trim=[[80, 70, 110, 105], [50, 80, 65, 85]])
+    
+    fig = plt.figure()
+    plot_utils.plot_WISPR(composite, wcs=wcs, grid=1)
+    return fig
+
+
+def test_gen_composite_trim_defaults():
+    ifile, ofile, hdr_i, hdr_o = setup_gen_composite_test()
+    
+    composite, wcs = composites.gen_composite(ifile, ofile)
+    composite2, wcs2 = composites.gen_composite(ifile, ofile,
+            image_trim=[[None, None, None, None], [None, None, None, None]])
+    
+    np.testing.assert_equal(composite, composite2)
+    
+    assert wcs.to_header_string() == wcs2.to_header_string()
