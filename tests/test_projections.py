8148bfde521fc383ccdf3cd064f34c45dd95bd3f
Sam Van Kooten <vankooten.sam@gmail.com>
2022-06-28 12:45:03 -1000

    Add tests for complete radial projection

--- tests/test_projections.py
+++ tests/test_projections.py
@@ -1,6 +1,9 @@
 from .. import projections
 
 from astropy.wcs import WCS
+import matplotlib.pyplot as plt
+from matplotlib.testing.conftest import mpl_test_settings
+from matplotlib.testing.decorators import image_comparison
 import numpy as np
 import pytest
 from pytest import approx
@@ -237,3 +240,73 @@ def test_radial_transformer_roundtrip():
     pixel_in = inv_transformer(transformer(pixel_out))
     
     np.testing.assert_allclose(pixel_out, pixel_in)
+
+
+@image_comparison(baseline_images=['test_reproject_to_radial'],
+        extensions=['pdf'])
+def test_reproject_to_radial():
+    data = np.zeros((500, 500))
+    x = np.arange(-data.shape[0]//2, data.shape[0]//2)
+    y = np.arange(-data.shape[0]//2, data.shape[0]//2)
+    X, Y = np.meshgrid(x, y)
+    R = np.sqrt(X**2 + Y**2)
+    T = np.arctan2(Y, X) * 180 / np.pi
+
+    for r in range(10, data.shape[0], 50):
+        data[(R > r-10) * (R < r+10)] = 1
+
+    for t in range(-210, 210, 30):
+        data[(T > t-4) * (T < t+4)] = 1
+    
+    data *= (1 + T / 365)
+
+    wcs_in = WCS(naxis=2)
+    wcs_in.wcs.ctype = 'HPLN-ARC', 'HPLT-ARC'
+    wcs_in.wcs.crpix = data.shape[0]//2, data.shape[1]//2
+    wcs_in.wcs.crval = 0, 0
+    wcs_in.wcs.cdelt = .3, .3
+
+    out, trans = projections.reproject_to_radial(
+            data, wcs_in, out_shape=(433, 500), delongation=.39,
+            ref_elongation=0, ref_x=250, ref_pa=0, dpa=.84, ref_y=217)
+    
+    plt.imshow(out, cmap='viridis', vmin=0)
+    
+    projections.label_radial_axes(trans)
+    
+    return plt.gcf()
+
+
+@image_comparison(baseline_images=['test_reproject_from_radial'],
+        extensions=['pdf'])
+def test_reproject_from_radial():
+    data = np.zeros((500, 500))
+    x = np.arange(0, data.shape[0])
+    y = np.arange(0, data.shape[1])
+    X, Y = np.meshgrid(x, y)
+
+    for x in range(0, data.shape[1]+1, int(30/.39)):
+        data[(X > x-3) * (X < x+3)] = 1
+
+    for y in range(0, data.shape[0]+1, int(30/.84)):
+        data[(Y > y-3) * (Y < y+3)] = 1
+
+    data *= (1 + X / 100)
+    
+    # Negative elongations should not be used
+    data[:, :250] = 9
+
+    wcs_in = WCS(naxis=2)
+    wcs_in.wcs.ctype = 'HPLN-ARC', 'HPLT-ARC'
+    wcs_in.wcs.crpix = data.shape[0]//2, data.shape[1]//2
+    wcs_in.wcs.crval = 0, 0
+    wcs_in.wcs.cdelt = .3, .3
+
+    
+    out, itrans = projections.reproject_from_radial(data, wcs_in,
+            out_shape=data.shape, delongation=.39, ref_elongation=0,
+            ref_x=250, ref_pa=0, dpa=.84, ref_y=217)
+    
+    plt.imshow(out, cmap='viridis', vmin=0)
+    
+    return plt.gcf()
