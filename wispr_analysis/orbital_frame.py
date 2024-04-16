d322fd89ce2a99a792630955e218a05a47053131
Sam Van Kooten <vankooten.sam@gmail.com>
2024-04-16 13:56:13 -0600

    Rework orbital frame code, make more rigorous

--- wispr_analysis/orbital_frame.py
+++ wispr_analysis/orbital_frame.py
@@ -1,34 +1,26 @@
+import sunpy.coordinates.frameattributes
 import astropy.coordinates
 import astropy.units as u
 import astropy.wcs
 import numpy as np
 import spiceypy as spice
+import sunpy.coordinates
 
 from . import planets
 
 
 class PSPOrbitalFrame(astropy.coordinates.BaseCoordinateFrame):
     """
-    This is a frame that represents a celestial sphere with its equator aligned
-    with PSP's orbital plane.
+    This is a heliocentric frame with its equator aligned with PSP's orbit plane
     
     This is a time-varying frame, as PSP's orbital plane changes with every
     Venus encounter.
     
-    Note: This frame does not attempt to account carefully for the origin of
-    the coordinate system, and whether that is the Sun or PSP, etc. Formally,
-    the origin is the barycenter of the solar system, as it is just a rotated
-    ICRS frame. In practice, I use this frame with PSP taken as the
-    origin---this does not affect where the stars appear due to their distance,
-    and allows us to project into an unrotating, PSP-following frame.
+    This frame is registered under the WCS keys POLN and POLT.
     
-    The zero point of longitude is set so that, when a full encounter is
-    reprojected into this frame, it conveniently spans a range centered in
-    [0, 360] without wrapping around.
-
     Parameters
     ----------
-    obstime : str
+    obstime
         The time of the observation, defining the PSP orbital plane.
     """
     default_representation = astropy.coordinates.representation.SphericalRepresentation
@@ -37,14 +29,13 @@ class PSPOrbitalFrame(astropy.coordinates.BaseCoordinateFrame):
 
 @astropy.coordinates.frame_transform_graph.transform(
     astropy.coordinates.DynamicMatrixTransform,
-    astropy.coordinates.ICRS,
+    sunpy.coordinates.HeliocentricInertial,
     PSPOrbitalFrame)
-def icrs_to_psp(ICRScoord, PSPframe):
-    obstime = (PSPframe.obstime
-               or getattr(ICRScoord, 'obstime', '2020-01-01T01:01:01'))
+def hci_to_psp_orbital(HCIcoord, PSPframe):
+    obstime = PSPframe.obstime or HCIcoord.obstime
     (perifocal_distance, eccentricity, inclination,
             lon_asc_node, arg_periapsis) = planets.get_orbital_elements(
-            obstime, 'psp', coord_frame='J2000')
+            obstime, 'psp', coord_frame='HCI')
     
     # Rotate around the ascending-node axis by inclination
     matrix = spice.axisar(
@@ -52,9 +43,7 @@ def icrs_to_psp(ICRScoord, PSPframe):
         -inclination.to(u.rad).value)
     
     # Rotate around the vertical axis to position zero longitude conveniently
-    matrix2 = spice.axisar(
-        [0, 0, 1],
-        -87.5 * np.pi/180)
+    matrix2 = spice.axisar([0, 0, 1], -10 * np.pi/180)
     matrix = matrix2 @ matrix
     
     return matrix
@@ -63,18 +52,15 @@ def icrs_to_psp(ICRScoord, PSPframe):
 @astropy.coordinates.frame_transform_graph.transform(
     astropy.coordinates.DynamicMatrixTransform,
     PSPOrbitalFrame,
-    astropy.coordinates.ICRS)
-def psp_to_icrs(PSPcoord, ICRSframe):
-    obstime = (PSPcoord.obstime
-               or getattr(ICRSframe, 'obstime', '2020-01-01T01:01:01'))
+    sunpy.coordinates.HeliocentricInertial)
+def psp_orbital_to_hci(PSPcoord, HCIframe):
+    obstime = PSPcoord.obstime or HCIframe.obstime
     (perifocal_distance, eccentricity, inclination,
             lon_asc_node, arg_periapsis) = planets.get_orbital_elements(
-            obstime, 'psp', coord_frame='J2000')
+            obstime, 'psp', coord_frame='HCI')
     
     # Rotate around the vertical axis to put zero longitude where it should be
-    matrix = spice.axisar(
-        [0, 0, 1],
-        87.5 * np.pi/180)
+    matrix = spice.axisar([0, 0, 1], 10 * np.pi/180)
     
     # Rotate around the ascending-node axis by inclination
     matrix2 = spice.axisar(
@@ -85,31 +71,201 @@ def psp_to_icrs(PSPcoord, ICRSframe):
     return matrix
 
 
+class PSPFrame(astropy.coordinates.BaseCoordinateFrame):
+    """
+    This is an observer-centric frame with its equator aligned with PSP's
+    orbital plane, and with longitude fixed to an inertial reference point (i.e.
+    fixed to the stars).
+    
+    This is a time-varying frame, as PSP's orbital plane changes with every
+    Venus encounter.
+    
+    This frame is registered under the WCS keys PSLN and PSLT.
+
+    Parameters
+    ----------
+    obstime
+        The time of the observation, defining the PSP orbital plane.
+    observer
+        The observer, defining the origin of the coordinate system.
+    """
+    default_representation = astropy.coordinates.representation.SphericalRepresentation
+    obstime = astropy.coordinates.TimeAttribute()
+    observer = sunpy.coordinates.frameattributes.ObserverCoordinateAttribute(
+        sunpy.coordinates.HeliographicStonyhurst)
+
+
+@astropy.coordinates.frame_transform_graph.transform(
+    astropy.coordinates.DynamicMatrixTransform,
+    sunpy.coordinates.Helioprojective,
+    PSPFrame)
+def hpc_to_psp(HPcoord, PSPframe):
+    if not _observers_are_equal(HPcoord.observer, PSPframe.observer):
+        raise ValueError("Observers are not equal")
+    if not _times_are_equal(HPcoord.obstime, PSPframe.obstime):
+        raise ValueError("Obstimes are not equal")
+    obstime = HPcoord.obstime or PSPframe.obstime
+    observer = HPcoord.observer or PSPframe.observer
+    orbital_north = astropy.coordinates.SkyCoord(
+        0*u.m, 0*u.m, 1000*u.m, representation_type='cartesian',
+        frame=PSPOrbitalFrame, obstime=obstime, observer=observer)
+    orbital_north = orbital_north.transform_to('helioprojective')
+    angle = np.arctan2(orbital_north.Tx, orbital_north.Ty)
+    
+    matrix = spice.axisar([1, 0, 0], angle.to_value(u.rad))
+    
+    observer = observer.transform_to(PSPOrbitalFrame(obstime=obstime))
+    lon = observer.lon - 180 * u.deg
+    matrix2 = spice.axisar([0, 0, 1], -lon.to_value(u.rad))
+    matrix = matrix2 @ matrix
+    
+    return matrix
+
+
+@astropy.coordinates.frame_transform_graph.transform(
+    astropy.coordinates.DynamicMatrixTransform,
+    PSPFrame,
+    sunpy.coordinates.Helioprojective)
+def psp_to_hpc(PSPcoord, HPframe):
+    if not _observers_are_equal(PSPcoord.observer, HPframe.observer):
+        raise ValueError("Observers are not equal")
+    if not _times_are_equal(PSPcoord.obstime, HPframe.obstime):
+        raise ValueError("Obstimes are not equal")
+    obstime = PSPcoord.obstime or HPframe.obstime
+    observer = PSPcoord.observer or HPframe.observer
+    orbital_north = astropy.coordinates.SkyCoord(
+        0*u.m, 0*u.m, 1000*u.m, representation_type='cartesian',
+        frame=PSPOrbitalFrame, obstime=obstime, observer=observer)
+    orbital_north = orbital_north.transform_to('helioprojective')
+    angle = np.arctan2(orbital_north.Tx, orbital_north.Ty)
+    
+    matrix = spice.axisar([1, 0, 0], -angle.to_value(u.rad))
+    
+    observer = observer.transform_to(PSPOrbitalFrame(obstime=obstime))
+    lon = observer.lon - 180 * u.deg
+    matrix2 = spice.axisar([0, 0, 1], lon.to_value(u.rad))
+    matrix = matrix @ matrix2
+    
+    return matrix
+
+
 def orbital_plane_wcs_frame_mapping(wcs):
     ctypes = {c[:4] for c in wcs.wcs.ctype}
-    if not ({'PSLN', 'PSLT'} <= ctypes):
-        return None
-    
-    dateobs = wcs.wcs.dateavg or wcs.wcs.dateobs or None
-    orbital_frame = PSPOrbitalFrame(obstime=dateobs)
-    return orbital_frame
+    if ({'PSLN', 'PSLT'} <= ctypes):
+        dateobs = wcs.wcs.dateavg or wcs.wcs.dateobs or None
+        observer = _get_observer_from_wcs(wcs, dateobs)
+        return PSPFrame(obstime=dateobs, observer=observer)
+    if ({'POLN', 'POLT'} <= ctypes):
+        dateobs = wcs.wcs.dateavg or wcs.wcs.dateobs or None
+        return PSPOrbitalFrame(obstime=dateobs)
+    return None
 
 
 def orbital_plane_frame_wcs_mapping(frame, projection='TAN'):
-    if not isinstance(frame, PSPOrbitalFrame):
-        return None
-    wcs = astropy.wcs.WCS(naxis=2)
-    if frame.obstime:
-        wcs.wcs.dateobs = frame.obstime.utc.isot
-    wcs.wcs.ctype = f'PSLN-{projection}', f'PSLT-{projection}'
-    wcs.wcs.cunit = ['deg', 'deg']
-    return wcs
+    if isinstance(frame, PSPOrbitalFrame):
+        wcs = astropy.wcs.WCS(naxis=2)
+        if frame.obstime:
+            wcs.wcs.dateobs = frame.obstime.utc.isot
+        wcs.wcs.ctype = f'POLN-{projection}', f'POLT-{projection}'
+        wcs.wcs.cunit = ['deg', 'deg']
+        return wcs
+    
+    if isinstance(frame, PSPFrame):
+        wcs = astropy.wcs.WCS(naxis=2)
+        if frame.obstime:
+            wcs.wcs.dateobs = frame.obstime.utc.isot
+        _set_wcs_aux_obs_coord(wcs, frame.observer)
+        wcs.wcs.ctype = f'PSLN-{projection}', f'PSLT-{projection}'
+        wcs.wcs.cunit = ['deg', 'deg']
+        return wcs
+    return None
 
 
 astropy.wcs.utils.WCS_FRAME_MAPPINGS.append([orbital_plane_wcs_frame_mapping])
 astropy.wcs.utils.FRAME_WCS_MAPPINGS.append([orbital_plane_frame_wcs_mapping])
+# TODO: Once astropy releases with PR 15626, these can/should get "custom:"
+# prefixes
 astropy.wcs.wcsapi.fitswcs.CTYPE_TO_UCD1_CUSTOM.append(
     {
-        "PSLN": "custom:pos.psporbitalplane.lon",
-        "PSLT": "custom:pos.psporbitalplane.lat",
+        "PSLN": "pos.pspframe.lon",
+        "PSLT": "pos.pspframe.lat",
+        "POLN": "pos.psporbitalframe.lon",
+        "POLT": "pos.psporbitalframe.lat",
     })
+
+
+def _get_observer_from_wcs(wcs, dateobs):
+    # Ripped from sunpy
+    rsun = wcs.wcs.aux.rsun_ref
+    if rsun is not None:
+        rsun *= u.m
+    
+    required_attrs = {sunpy.coordinates.HeliographicStonyhurst:
+                        ['hgln_obs', 'hglt_obs', 'dsun_obs'],
+                      sunpy.coordinates.HeliographicCarrington:
+                        ['crln_obs', 'hglt_obs', 'dsun_obs']}
+    
+    observer = None
+    for frame, attr_names in required_attrs.items():
+        attrs = [getattr(wcs.wcs.aux, attr_name) for attr_name in attr_names]
+        if all([attr is not None for attr in attrs]):
+            kwargs = {'obstime': dateobs}
+            if rsun is not None:
+                kwargs['rsun'] = rsun
+            if issubclass(frame, sunpy.coordinates.HeliographicCarrington):
+                kwargs['observer'] = 'self'
+
+            observer = frame(attrs[0] * u.deg,
+                             attrs[1] * u.deg,
+                             attrs[2] * u.m,
+                             **kwargs)
+            return observer
+
+
+def _set_wcs_aux_obs_coord(wcs, obs_frame):
+    # Ripped from sunpy
+    # Sometimes obs_coord can be a SkyCoord, so convert down to a frame
+    if hasattr(obs_frame, 'frame'):
+        obs_frame = obs_frame.frame
+
+    if isinstance(obs_frame, sunpy.coordinates.HeliographicStonyhurst):
+        wcs.wcs.aux.hgln_obs = obs_frame.lon.to_value(u.deg)
+    elif isinstance(obs_frame, sunpy.coordinates.HeliographicCarrington):
+        wcs.wcs.aux.crln_obs = obs_frame.lon.to_value(u.deg)
+    else:
+        raise ValueError('obs_coord must be in a Stonyhurst or Carrington frame')
+    # These two keywords are the same for Carrington and Stonyhurst
+    wcs.wcs.aux.hglt_obs = obs_frame.lat.to_value(u.deg)
+    wcs.wcs.aux.dsun_obs = obs_frame.radius.to_value(u.m)
+
+
+def _observers_are_equal(obs_1, obs_2):
+    # Ripped from sunpy, modified
+    if obs_1 is None and obs_2 is None:
+        raise ValueError("An observer must be set for this transformation.")
+    
+    if obs_1 is obs_2:
+        return True
+
+    # obs_1 != obs_2
+    if obs_1 is None:
+        return True
+    if obs_2 is None:
+        return True
+    
+    return np.atleast_1d(u.allclose(obs_1.lat, obs_2.lat) and
+                         u.allclose(obs_1.lon, obs_2.lon) and
+                         u.allclose(obs_1.radius, obs_2.radius) and
+                         _times_are_equal(obs_1.obstime, obs_2.obstime)).all()
+
+
+def _times_are_equal(time_1, time_2):
+    # Ripped from sunpy, modified
+    # Checks whether times are equal
+    if isinstance(time_1, astropy.time.Time) and isinstance(time_2, astropy.time.Time):
+        # We explicitly perform the check in TAI to avoid possible numerical precision differences
+        # between a time in UTC and the same time after a UTC->TAI->UTC conversion
+        return np.all(time_1.tai == time_2.tai)
+
+    # We also deem the times equal if one is None
+    return time_1 is None or time_2 is None
--- wispr_analysis/tests/test_orbital_frame.py
+++ wispr_analysis/tests/test_orbital_frame.py
@@ -1,11 +1,11 @@
-import os
-
 import astropy.coordinates
 import astropy.units as u
 import astropy.wcs
 import numpy as np
+import pytest
+import sunpy.coordinates
 
-from .. import planets, orbital_frame, utils
+from .. import orbital_frame
 
 
 def test_orbital_plane_wcs_frame_mapping():
@@ -18,6 +18,12 @@ def test_orbital_plane_wcs_frame_mapping():
     wcs.wcs.cdelt = -360 / nx, 45 / ny
     wcs.wcs.cunit = 'deg', 'deg'
     wcs.wcs.dateobs = '2021-01-01 12:12:12'
+    
+    frame = orbital_frame.orbital_plane_wcs_frame_mapping(wcs)
+    assert isinstance(frame, orbital_frame.PSPFrame)
+    assert frame.obstime == wcs.wcs.dateobs
+    
+    wcs.wcs.ctype = 'POLN-ARC', 'POLT-ARC'
     
     frame = orbital_frame.orbital_plane_wcs_frame_mapping(wcs)
     assert isinstance(frame, orbital_frame.PSPOrbitalFrame)
@@ -40,6 +46,12 @@ def test_coord_from_wcs():
     wcs.wcs.dateobs = '2021-01-01 12:12:12'
     
     coord = wcs.pixel_to_world(1,1)
+    assert isinstance(coord.frame, orbital_frame.PSPFrame)
+    assert coord.frame.obstime == wcs.wcs.dateobs
+    
+    wcs.wcs.ctype = 'POLN-ARC', 'POLT-ARC'
+    
+    coord = wcs.pixel_to_world(1,1)
     assert isinstance(coord.frame, orbital_frame.PSPOrbitalFrame)
     assert coord.frame.obstime == wcs.wcs.dateobs
 
@@ -48,22 +60,57 @@ def test_orbital_plane_frame_wcs_mapping():
     obstime = '2022-02-03T12:12:12.000'
     frame = orbital_frame.PSPOrbitalFrame(obstime=obstime)
     wcs = orbital_frame.orbital_plane_frame_wcs_mapping(frame, 'CAR')
+    assert list(wcs.wcs.ctype) == ['POLN-CAR', 'POLT-CAR']
+    assert wcs.wcs.dateobs == obstime
+    assert wcs.world_axis_physical_types == [
+        'custom:pos.psporbitalframe.lon',
+        'custom:pos.psporbitalframe.lat']
+
+
+def test_psp_frame_wcs_mapping():
+    obstime = '2022-02-03T12:12:12.000'
+    observer = astropy.coordinates.SkyCoord(10*u.deg, 20*u.deg, 30*u.R_sun,
+                                            frame='heliographic_stonyhurst')
+    frame = orbital_frame.PSPFrame(obstime=obstime, observer=observer)
+    wcs = orbital_frame.orbital_plane_frame_wcs_mapping(frame, 'CAR')
     assert list(wcs.wcs.ctype) == ['PSLN-CAR', 'PSLT-CAR']
     assert wcs.wcs.dateobs == obstime
     assert wcs.world_axis_physical_types == [
-        'custom:pos.psporbitalplane.lon',
-        'custom:pos.psporbitalplane.lat']
+        'custom:pos.pspframe.lon',
+        'custom:pos.pspframe.lat']
+    assert wcs.wcs.aux.hgln_obs == observer.lon.to_value(u.deg)
+    assert wcs.wcs.aux.hglt_obs == observer.lat.to_value(u.deg)
+    assert wcs.wcs.aux.dsun_obs == observer.radius.to_value(u.m)
 
 
-def test_wcs_from_coord():
+def test_wcs_from_psporbital_coord():
     obstime = '2022-02-03T12:12:12.000'
     frame = orbital_frame.PSPOrbitalFrame(obstime=obstime)
     wcs = astropy.wcs.utils.celestial_frame_to_wcs(frame)
+    assert list(wcs.wcs.ctype) == ['POLN-TAN', 'POLT-TAN']
+    assert wcs.wcs.dateobs == obstime
+    assert wcs.world_axis_physical_types == [
+        'custom:pos.psporbitalframe.lon',
+        'custom:pos.psporbitalframe.lat']
+
+
+def test_wcs_from_psp_coord():
+    obstime = '2022-02-03T12:12:12.000'
+    observer = astropy.coordinates.SkyCoord(10*u.deg, 20*u.deg, 30*u.R_sun,
+                                            frame='heliographic_stonyhurst')
+    frame = orbital_frame.PSPFrame(obstime=obstime, observer=observer)
+    wcs = astropy.wcs.utils.celestial_frame_to_wcs(frame)
     assert list(wcs.wcs.ctype) == ['PSLN-TAN', 'PSLT-TAN']
     assert wcs.wcs.dateobs == obstime
+    assert wcs.world_axis_physical_types == [
+        'custom:pos.pspframe.lon',
+        'custom:pos.pspframe.lat']
+    assert wcs.wcs.aux.hgln_obs == observer.lon.to_value(u.deg)
+    assert wcs.wcs.aux.hglt_obs == observer.lat.to_value(u.deg)
+    assert wcs.wcs.aux.dsun_obs == observer.radius.to_value(u.m)
 
 
-def test_psp_icrs_roundtrip(mocker):
+def test_psporbital_hci_roundtrip(mocker):
     # The PSP kernels are very large and not included in this repo
     mocker.patch('wispr_analysis.orbital_frame.planets.get_orbital_elements',
                 return_value=(
@@ -79,7 +126,7 @@ def test_psp_icrs_roundtrip(mocker):
         10*u.deg, 30*u.deg, 5*u.au, frame='psporbitalframe',
         obstime=obstime)
     
-    c2 = c.transform_to('icrs')
+    c2 = c.transform_to('heliocentricinertial')
     
     c3 = c2.transform_to('psporbitalframe')
     
@@ -88,14 +135,48 @@ def test_psp_icrs_roundtrip(mocker):
     assert np.isclose(c.distance, c3.distance)
     assert c3.obstime == obstime
     
-    c4 = c3.transform_to('icrs')
+    c4 = c3.transform_to('heliocentricinertial')
     
-    assert np.isclose(c2.ra, c4.ra)
-    assert np.isclose(c2.dec, c4.dec)
+    assert np.isclose(c2.lon, c4.lon)
+    assert np.isclose(c2.lat, c4.lat)
     assert np.isclose(c2.distance, c4.distance)
 
 
-def test_icrs_psp_conversion(mocker):
+def test_psp_hpc_roundtrip(mocker):
+    # The PSP kernels are very large and not included in this repo
+    mocker.patch('wispr_analysis.orbital_frame.planets.get_orbital_elements',
+                return_value=(
+                    19390313.93764631 * u.km,
+                    0.741835928443799,
+                    0.42649992 * u.rad,
+                    0.13972456 * u.rad,
+                    2.56225543 * u.rad,
+                    ))
+    
+    obstime = '2023-01-02 03:04:05'
+    observer = astropy.coordinates.SkyCoord(10*u.deg, 20*u.deg, 30*u.R_sun,
+                                            frame='heliographic_stonyhurst')
+    c = astropy.coordinates.SkyCoord(
+        10*u.deg, 30*u.deg, 5*u.au, frame='pspframe',
+        obstime=obstime, observer=observer)
+    
+    c2 = c.transform_to('helioprojective')
+    
+    c3 = c2.transform_to('pspframe')
+    
+    assert np.isclose(c.lon, c3.lon)
+    assert np.isclose(c.lat, c3.lat)
+    assert np.isclose(c.distance, c3.distance)
+    assert c3.obstime == obstime
+    
+    c4 = c3.transform_to('helioprojective')
+    
+    assert np.isclose(c2.Tx, c4.Tx)
+    assert np.isclose(c2.Ty, c4.Ty)
+    assert np.isclose(c2.distance, c4.distance)
+
+
+def test_hci_psporbital_conversion(mocker):
     # The PSP kernels are very large and not included in this repo
     mocker.patch('wispr_analysis.orbital_frame.planets.get_orbital_elements',
                 return_value=(
@@ -108,16 +189,16 @@ def test_icrs_psp_conversion(mocker):
     
     obstime = '2019-09-09 01:01:01'
     c = astropy.coordinates.SkyCoord(80*u.deg, -10*u.deg, 33*u.au,
-                 frame='icrs', obstime=obstime)
+                 frame='heliocentricinertial', obstime=obstime)
     c = c.transform_to('psporbitalframe')
     
-    assert np.isclose(c.lon.to_value(), 349.3277074789698)
+    assert np.isclose(c.lon.to_value(), 66.82770747896976)
     assert np.isclose(c.lat.to_value(), -32.27856834)
     assert np.isclose(c.distance.to_value(), 33)
     assert c.obstime == obstime
 
 
-def test_psp_icrs_conversion(mocker):
+def test_psporbital_hci_conversion(mocker):
     # The PSP kernels are very large and not included in this repo
     mocker.patch('wispr_analysis.orbital_frame.planets.get_orbital_elements',
                 return_value=(
@@ -131,9 +212,76 @@ def test_psp_icrs_conversion(mocker):
     obstime = '2019-09-09 01:01:01'
     c = astropy.coordinates.SkyCoord(80*u.deg, -10*u.deg, 33*u.au,
                  frame='psporbitalframe', obstime=obstime)
-    c = c.transform_to('icrs')
+    c = c.transform_to('heliocentricinertial')
     
-    assert np.isclose(c.ra.to_value(), 165.3498440482125)
-    assert np.isclose(c.dec.to_value(), -1.035504901881233)
+    assert np.isclose(c.lon.to_value(), 89.89502647497844)
+    assert np.isclose(c.lat.to_value(), 13.408571443919827)
     assert np.isclose(c.distance.to_value(), 33)
     assert c.obstime == obstime
+
+
+def test_hpc_psp_conversion(mocker):
+    # The PSP kernels are very large and not included in this repo
+    mocker.patch('wispr_analysis.orbital_frame.planets.get_orbital_elements',
+                return_value=(
+                    24817701.61920938 * u.km,
+                    0.6994548150099811,
+                    0.41291772 * u.rad,
+                    0.14661706 * u.rad,
+                    2.42928699 * u.rad,
+                    ))
+    
+    obstime = '2019-09-09 01:01:01'
+    observer = astropy.coordinates.SkyCoord(10*u.deg, 20*u.deg, 30*u.R_sun,
+                                            frame='heliographic_stonyhurst')
+    c = astropy.coordinates.SkyCoord(80*u.deg, -10*u.deg, 33*u.au,
+                 frame='helioprojective', obstime=obstime, observer=observer)
+    c = c.transform_to('pspframe')
+    
+    assert np.isclose(c.lon.to_value(), 339.4265894441424)
+    assert np.isclose(c.lat.to_value(), -10.901241566609368)
+    assert np.isclose(c.distance.to_value(), 33)
+    assert c.obstime == obstime
+
+
+def test_psp_hpc_conversion(mocker):
+    # The PSP kernels are very large and not included in this repo
+    mocker.patch('wispr_analysis.orbital_frame.planets.get_orbital_elements',
+                return_value=(
+                    24817701.61920938 * u.km,
+                    0.6994548150099811,
+                    0.41291772 * u.rad,
+                    0.14661706 * u.rad,
+                    2.42928699 * u.rad,
+                    ))
+    
+    obstime = '2019-09-09 01:01:01'
+    observer = astropy.coordinates.SkyCoord(10*u.deg, 20*u.deg, 30*u.R_sun,
+                                            frame='heliographic_stonyhurst')
+    c = astropy.coordinates.SkyCoord(80*u.deg, -10*u.deg, 33*u.au,
+                 frame='pspframe', obstime=obstime, observer=observer)
+    c = c.transform_to('helioprojective')
+    
+    assert np.isclose(c.Tx.to_value(), -646622.5542439144)
+    assert np.isclose(c.Ty.to_value(), -36026.64360996204)
+    assert np.isclose(c.distance.to_value(), 33)
+    assert c.obstime == obstime
+
+
+def test_psp_hpc_mismatched_observers():
+    obstime = '2019-09-09 01:01:01'
+    observer1 = astropy.coordinates.SkyCoord(10*u.deg, 20*u.deg, 30*u.R_sun,
+                                            frame='heliographic_stonyhurst')
+    observer2 = astropy.coordinates.SkyCoord(20*u.deg, 20*u.deg, 30*u.R_sun,
+                                            frame='heliographic_stonyhurst')
+    c = astropy.coordinates.SkyCoord(80*u.deg, -10*u.deg, 33*u.au,
+                 frame='pspframe', obstime=obstime, observer=observer1)
+    with pytest.raises(ValueError, match="Observers are not equal"):
+        c.transform_to(sunpy.coordinates.Helioprojective(
+            obstime=obstime, observer=observer2))
+    
+    c = astropy.coordinates.SkyCoord(80*u.deg, -10*u.deg, 33*u.au,
+                 frame='helioprojective', obstime=obstime, observer=observer1)
+    with pytest.raises(ValueError, match="Observers are not equal"):
+        c.transform_to(orbital_frame.PSPFrame(
+            obstime=obstime, observer=observer2))
